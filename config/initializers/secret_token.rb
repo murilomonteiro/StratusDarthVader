e175d4a8baf8deed12bbef8bafccfe3648532f8c
Murilo Monteiro <murilomonteiro@Murilos-MacBook-Pro.local>
2012-03-20 14:24:48 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DarthvaderSoundcloudPlayer::Application.config.secret_token = 'd01ab485870476c981a699aaaf181691f2105bad49a65220e97c1a22a9865f4f38837d3138c07f24d5fe2448b36c6a0d31ec7c792237e4469e00847413b68397'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DarthvaderSoundcloudPlayer::Application.config.session_store :cookie_store, key: '_darthvader_soundcloud_player_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DarthvaderSoundcloudPlayer::Application.config.session_store :active_record_store
